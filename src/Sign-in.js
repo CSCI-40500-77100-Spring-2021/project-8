a7125f5580e501ccfa49f1261bb611f2b4a61ec7
ETrsw <78511310+ETrsw@users.noreply.github.com>
2021-03-24 19:20:28 -0400

    Add files via upload

--- src/Sign-in.js
+++ src/Sign-in.js
@@ -2,18 +2,63 @@ import React from "react";
 import Field from "./field";
 import { TextField } from "@material-ui/core";
 
-const signin = (action) => {
+
+const handlelogout = async e => {
+  e.preventDefault();
+  try {//remove jwt toke when logged out
+    localStorage.storage.removeItem("token");
+    //unauthorize user 
+    //go to /login or other page
+  } catch (logouterror) {
+    console.error(logouterror.message);
+  }
+};
+const Signin = (action) => {
+
+//set state for username/email and password
+  const [username, setUser] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const onSubmit= async e => { 
+
+    e.preventDefault();
+   
+    try {
+    const body = { username, password };
+    //onsubmit json.stringify the username/passowrd 
+   const  response = await fetch("/auth/login",
+   {//post request to http://localhost:5000/auth/login or  just /auth/login for heroku
+
+     method: "POST",
+     headers: {"Content-type": "application/json" },
+     body: JSON.stringify(body),
+   }); 
+    //if it returns a jwttoken save jwttoken in local storage else login failed
+   const parseRes = await response.json();
+   if(parseRes.jwtToken)  {localStorage.setItem("token", parseRes.jwtToken); }
+   
+//go somewhere if logged in
+
+} 
+ 
+ catch (loginerror) {
+   console.error(loginerror.message);
+   
+ }
+}
   let id = 0;
   id++;
   return (
-    <form onSubmit={action}>
+    <form onSubmit={onSubmit}>
       <div className="form-group">
         <TextField
           id={"SignUp" + id++}
           name="username"
           type="text"
+          value ={username}
           placeholder="ex. john.smith"
           label="username"
+          onChange ={ e => setUser(e.target.value)}
         />
       </div>
 
@@ -22,8 +67,10 @@ const signin = (action) => {
           id={"SignUp" + id++}
           name="password"
           type="password"
+          value={password}
           placeholder="******"
           label="password"
+          onChange={e => setPassword(e.target.value)}
         />
       </div>
 
@@ -39,4 +86,4 @@ const signin = (action) => {
   );
 };
 
-export default signin;
+export default Signin;
